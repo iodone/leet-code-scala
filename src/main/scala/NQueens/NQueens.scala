447da204e6794e5e5853ba5e051fafa74fbe1ad5
zhangyaodong <zhangyaodong@antiy.cn>
2019-05-22 12:02:18 +0800

    slove nqueens

---
+++ src/main/scala/NQueens/NQueens.scala
@@ -0,0 +1,41 @@
+package NQueens
+
+/**
+  * Created by iodone on {19-5-22}.
+  */
+
+object Solution {
+  def solveNQueens(n: Int): List[List[String]] = {
+    def placeQueens(k: Int): List[List[(Int, Int)]] = {
+      if (k == 0) List(List())
+      else
+        for {
+          queens <- placeQueens(k-1)
+          c <- 1 to n
+          queen = (k, c)
+          if isSafe(queen, queens)
+        } yield queens ::: queen :: Nil
+    }
+
+    def isSafe(queen: (Int, Int), queens: List[(Int, Int)]): Boolean = {
+      queens.forall(q => !inCheck(q, queen))
+    }
+
+    def inCheck(p: (Int, Int), q: (Int, Int)) = {
+      p._1 == q._1 || p._2 == q._2 || (p._1 - q._1).abs == (p._2 - q._2).abs
+    }
+
+    def queensToString(allSolution: List[List[(Int, Int)]]) = {
+      allSolution.map { queens =>
+        queens.map {
+         queen =>
+           val a = Array.fill(n)('.')
+           a(queen._2 - 1) = 'Q'
+           a.mkString
+        }
+      }
+    }
+    queensToString(placeQueens(n))
+  }
+
+}
---
+++ src/main/scala/NQueens/test.sc
@@ -0,0 +1,5 @@
+import NQueens.Solution._
+
+solveNQueens(4)
+solveNQueens(8)
+
