bf434893be01801e020926604a2065cba27942c1
zhangyaodong <zhangyaodong@antiy.cn>
2019-09-03 12:26:21 +0800

    complete regularexp matching

--- src/main/scala/RegularExpressionMatching/RegularExpressionMatching.scala
+++ src/main/scala/RegularExpressionMatching/RegularExpressionMatching.scala
@@ -20,5 +20,34 @@ package RegularExpressionMatching
 object Solution {
   def isMatch(s: String, p: String): Boolean = {
 
+    val dp = Array.fill(s.length+1, p.length+1)(false)
+    dp(0)(0) = true
+    for (j <- 1 to p.length) {
+      if (p(j-1) == '*' && dp(0)(j-2)) {
+        dp(0)(j) = true
+      }
+    }
+    for (i <- 1 to s.length; j <- 1 to p.length) {
+      if (s(i-1) == p(j-1) || p(j-1) == '.') {
+        dp(i)(j) = dp(i - 1)(j - 1)
+      } else if (p(j-1) == '*') {
+        if (s(i-1) != p(j-2)) {
+          dp(i)(j) = dp(i)(j-2)
+        }
+        if (s(i-1) == p(j-2) || p(j-2) == '.') {
+          dp(i)(j) = dp(i-1)(j) || dp(i)(j-1) || dp(i)(j-2)
+        }
+      }
+    }
+    dp(s.length)(p.length)
   }
+
+  def main(args: Array[String]): Unit = {
+    println(isMatch("aa", "a"))
+    println(isMatch("aa", "a*"))
+    println(isMatch("ab", ".*"))
+    println(isMatch("", "c*"))
+    println(isMatch("aab", "c*a*b"))
+  }
+
 }
